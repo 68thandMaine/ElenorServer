b521bf6feb28c5e579d6a52958e399abed5c7f4d
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-06-24 12:18:54 -0700

    add: testing project for Messages controller

---
+++ ElenorServer/ElenorServer.Tests/ControllerTests/MessagesControllerTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ElenorServer.Controllers;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace ElenorServer.Tests
+{
+    [TestClass]
+    public class MessagesTest : IDisposable
+    {
+        public void Dispose()
+        {
+            Messages.ClearAll();
+        }
+
+        public MessagesTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=elenor_test;";
+        }
+    }
+}
