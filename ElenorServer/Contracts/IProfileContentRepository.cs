994f93aa6f7d498619ec1dd798f4f810558f424d
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-11-01 19:49:02 -0700

    Add an interface for profile content

---
+++ ElenorServer/Contracts/IProfileContentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.Models;
+
+namespace Contracts
+{
+    public interface IProfileContentRepository : IRepositoryBase<ProfileContent>
+    {
+        List<ProfileContent> GetProfileContent();
+        void CreateProfile(ProfileContent profileContent);
+        void DeleteProfile(ProfileContent profileContent);
+    }
+}
